51e3b67eb3f13854ffcd81f66f7a10514e6eedef
Wade Fagen-Ulmschneider <waf@illinois.edu>
2018-03-25 18:39:58 -0500

    added TRE dataset

---
+++ teachers-ranked-as-excellent/README.md
@@ -0,0 +1,58 @@
+# Teachers Ranked as Excellent at The University of Illinois
+
+A collection of all "Teachers Ranked as Excellent" at UIUC from Fall 1993 through Summer 2017 in a clean, consistent CSV format.
+
+[A single CSV containing all of the data can be downloaded here.][Single_CSV_Download]
+
+[Single_CSV_Download]: https://raw.githubusercontent.com/wadefagen/datasets/master/teachers-ranked-as-excellent/uiuc-tre-dataset.csv
+
+## Background
+
+At the University of Illinois at Urbana-Champaign, every semester students complete an [Instructor and Course Evaluation (ICES)][ICES_CITL] questionnaire for each course they are enrolled.  The [Center for Innovation in Teaching and Learning (CITL)][MAIN_CITL] compiles this data into the list of "Teachers Ranked as Excellent" and publishes the names of those teaches in the local newspaper and [online as a PDF][TRE_CITL].
+
+[TRE_CITL]: http://citl.illinois.edu/citl-101/measurement-evaluation/teaching-evaluation/teaching-evaluations-(ices)/teachers-ranked-as-excellent
+[ICES_CITL]: http://citl.illinois.edu/citl-101/measurement-evaluation/teaching-evaluation/teaching-evaluations-(ices)
+[MAIN_CITL]: http://citl.illinois.edu/
+
+## Repository Contents
+
+This repository contains a collection of all known lists of "Teachers Ranked as Excellent", dating back to 1993, in a consistent, documented CSV format.  Each semester can be found in the `csv` directory.
+
+The goal of this repository is to provide an easy interface for the use of this data in data science projects.
+
+### CSV Fields
+
+All CSVs contain eight fields, with field labels in the first row and data in all following rows.  The data contained in the fields are as follows:
+
+- `term`: Two letter semester code (`sp`, `su`, `fa`, or `wi`) followed by a four digit year.
+  * Examples: `sp2017`, `fa2013`, `su2012`.
+
+- `unit`: The CITL-supplied headers for the unit teaching the course.
+  * Examples: `ACCOUNTANCY`, `SOCIAL WORK`, `LINGUISTICS`, `NUCLEAR, PLASMA & RAD. ENGR.`
+
+- `lname`: The last name of the teacher.
+  * Examples: `FAGEN-ULMSCHNEIDER`, `FLANAGAN`, `FLECK`
+
+- `fname`: The first letter of the first name of the teacher.
+  * Examples: `W`, `K`, `M`
+
+- `role`: `Instructor` or `TA`
+
+- `ranking`: `Excellent` or `Outstanding`
+
+- `course`: The course the teacher was ranked as excellent.  If no course is given, the `course` field is set to `?` (this includes cases when the raw data lists the course as `0`, `000`, or `999`).
+  * Examples: `199`, `225`, `560`, `?`
+
+
+#### Ranked as Excellent in Multiple Courses in One Semester
+
+The same teacher may be ranked for multiple courses in a single semester.  In that case, the teacher will appear more than once.  For example:
+```
+sp2012,"NUCLEAR, PLASMA & RAD. ENGR.",AXFORD,R,Instructor,Excellent,447
+sp2012,"NUCLEAR, PLASMA & RAD. ENGR.",AXFORD,R,Instructor,Excellent,521
+```
+
+
+### Scripts
+
+The script used to generate this data can be found in `scripts`.
---
+++ teachers-ranked-as-excellent/script/index.js
@@ -0,0 +1,192 @@
+
+var rawTxtPath = "../raw_txt/";
+var rawCSVPath = "../csv/"
+
+
+var fs = require("fs");
+var path = require("path");
+String.prototype.contains = function(it) { return this.indexOf(it) != -1; };
+
+var files = fs.readdirSync(rawTxtPath);
+
+var containsCourseData = function(s) {
+  return s.match(/\d/) || s.indexOf("*") != -1;
+  //return /\d/.test(s);
+};
+
+
+var term_re = /(WINTER|SPRING|SUMMER|FALL).*(\d\d\d\d)/;
+var page_re = /(PAGE (\d+))|(^\d+$)/;
+
+var isPageBoundry = function (s) {
+  s = s.toUpperCase();
+
+  var containsSemester = s.match(term_re);
+  var containsPage = s.match(page_re);
+
+  if (containsSemester || containsPage) { return true; }
+  else { return false; }
+};
+
+var findTerm = function (s) {
+  s = s.toUpperCase();
+
+  var semester = term_re.exec(s)[1];
+  var year = term_re.exec(s)[2];
+
+  if (semester == "WINTER")      { semester = "wi"; }
+  else if (semester == "SPRING") { semester = "sp"; }
+  else if (semester == "SUMMER") { semester = "su"; }
+  else if (semester == "FALL")   { semester = "fa"; }
+  return semester + year;
+};
+
+var completeCSV = "";
+files.forEach(function (file) {
+  console.log("Working on: " + file);
+  var results = [];
+  var lines = fs.readFileSync(path.join(rawTxtPath, file), "utf8").split("\n");
+
+  var readingIntro = true;
+  var unitName = undefined;
+  var unitPersons = [];
+  var term = "?";
+
+  for (var i = 0; i < lines.length; i++) {
+    var line = lines[i].trim();
+    if (line.length == 0) { continue; }
+
+    if (readingIntro) {
+      line_l = line.toLowerCase();
+
+      // Spring 2017 format
+      if (line.contains("Page 2")) {
+        readingIntro = false;
+        if (term == "?") { term = findTerm(termStr); }
+        continue;
+      }
+
+      // Fall 2003 format
+      if (line_l.contains("based on data collected")) {
+        var termStr = line_l.substring(
+          line_l.indexOf("based on data collected") + "based on data collected".length + 1
+        ).trim();
+
+        termStr = termStr.substring(0, termStr.indexOf(")"));
+        term = findTerm(termStr);
+      }
+
+      if (line == "ACCOUNTANCY") {
+        readingIntro = false;
+      }
+    }
+
+    if (!readingIntro) {
+      if (isPageBoundry(line)) { continue; }
+
+      // When a unit name is `undefined`, the next thing we read is the unit name
+      if (unitName === undefined || !containsCourseData(line)) {
+        unitName = line;
+      }
+
+      else {
+        // If there's no numbers on a line, the courses are contained on the next line:
+        if (!line.match(/\d/)) {
+          line += " " + lines[++i].trim();
+        }
+
+        // Find the end of the name -- the space after the optional "* "
+        var endOfName = line.indexOf(",") + 2;
+        var firstDigit = /\d/.exec(line).index;
+
+
+
+        // Split the line into [name][courseString] segments
+        var name = line.substring(0, firstDigit).trim();
+        var courseString = line.substring(firstDigit).trim();
+
+        var personType = "Instructor";
+        if (name.substring(name.length - 2) == "TA") {
+          personType = "TA";
+          name = name.substring(0, name.length - 2).trim();
+        }
+
+        // Check if the name contians a * to denote "Outstanding"
+        var ranking = "Excellent";
+        if (name[0] == "*") {
+          name = name.substring(1);
+          ranking = "Outstanding";
+        }
+
+        //console.log(name);
+        nameSplit = name.split(",");
+        var fname = "";
+        if (nameSplit.length == 2) { fname = nameSplit[1].trim(); }
+        var lname = nameSplit[0].trim();
+
+        // Parse the coures
+        /*
+        var personType = "Instructor";
+        if (courseString.startsWith("TA")) {
+          personType = "TA";
+          courseString = courseString.substring(2).trim();
+        }
+        */
+
+        // In fa2000, teachers with more than two courses have a ","
+        // at the end of the line and their courses continue to the next line.
+        //
+        // For example in f00incl.txt :149,150
+        //      *KESLER,D                374,331,
+        //                               231,110
+        while (courseString[courseString.length - 1] == ",") {
+          courseString += lines[++i].trim();
+        }
+
+        var courses = [];
+        courseString.split(",").forEach(function (course) {
+          courses.push(course.trim());
+        });
+
+        // Create the object
+        var obj = {
+          fname: fname,
+          lname: lname,
+          ranking: ranking,
+          role: personType,
+          courses: courses,
+          unit: unitName,
+          term: term
+        };
+        results.push(obj);
+      }
+    }
+  }
+
+
+  // Write the output...
+  var csv = "term,unit,lname,fname,role,ranking,course\n";
+  if (completeCSV == "") { completeCSV = csv; }
+  results.forEach(function (obj) {
+    obj.courses.forEach(function (course) {
+      if (course == "0") { course = "?"; }
+      else if (course == "000") { course = "?"; }
+      else if (course == "999") { course = "?"; }
+
+      var str = obj.term + "," +
+                "\"" + obj.unit + "\"," +
+                obj.lname + "," +
+                obj.fname + "," +
+                obj.role + "," +
+                obj.ranking + "," +
+                course + "\n";
+
+      csv += str;
+      completeCSV += str;
+    })
+  });
+
+  fs.writeFileSync(path.join(rawCSVPath, "tre-" + term + ".csv"), csv);
+});
+
+fs.writeFileSync(path.join(rawCSVPath, "..", "TRE-UIUC-AllYears.csv"), completeCSV);
