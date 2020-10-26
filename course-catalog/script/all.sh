f836c62092790aa8e8891a6bd9bf7b19e38ed3ae
Wade Fagen-Ulmschneider <waf@illinois.edu>
2020-10-26 16:29:48 -0500

    courses data from 2016-fa -> 2020-fa

---
+++ course-catalog/script/all.sh
@@ -0,0 +1,7 @@
+node CIS-scraper.js 2019-fa
+node CIS-scraper.js 2019-sp
+node CIS-scraper.js 2018-fa
+node CIS-scraper.js 2018-sp
+node CIS-scraper.js 2017-fa
+node CIS-scraper.js 2017-sp
+node CIS-scraper.js 2016-fa
